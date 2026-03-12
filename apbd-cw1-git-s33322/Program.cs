dff28fe844a1171d04fd7ca2324d7799bbb48234
Maksym Wyrozębski <s33322@pjwstk.edu.pl>
2026-03-12 13:22:18 +0100

    new method 'CalculateAverage'

--- apbd-cw1-git-s33322/Program.cs
+++ apbd-cw1-git-s33322/Program.cs
@@ -21,4 +21,15 @@ double substract(double x, double y)
     return x - y;
 }
 
+double CalculateAverage(int[] values)
+{
+    double sum = 0;
+    foreach (int value in values)
+    {
+        sum += value;
+    }
+
+    return sum / values.Length;
+}
+
 Console.WriteLine("Hello, World!");
