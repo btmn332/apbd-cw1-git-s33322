edbdd88b8e0b7e5009b55aa2b32ee0bb20c2a726
Maksym <maksym.wyrozebski@gmail.com>
2026-03-18 22:47:37 +0100

    new Method 'CalculateMax'

--- apbd-cw1-git-s33322/Program.cs
+++ apbd-cw1-git-s33322/Program.cs
@@ -32,5 +32,17 @@ double CalculateAverage(int[] values)
     return sum / values.Length;
 }
 
+int CalculateMax(int[] values)
+{
+    int max = values[0];
+    foreach (int  value in values) {
+        if  (value > max) {
+            max = value;
+            }
+    }
+
+    return max;
+}
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("qwerty");
